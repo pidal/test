7f8346e592d4c0466460a1a6d94a04178782e84d
Alfonso Pidal <alfonsopidal@gmail.com>
2019-04-01 16:18:35 +0200

    initial comnmit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.pidal.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
